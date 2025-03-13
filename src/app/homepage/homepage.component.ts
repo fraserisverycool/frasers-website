3339e35c3427ecec0509d76686ba39a2cd3e4fe9
frazzle <fraserbowen82@hotmail.co.uk>
2025-03-13 01:15:09 +0100

    new albums and homepage pics

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -11,9 +11,27 @@ import {CustomMarqueeComponent} from "./custom-marquee/custom-marquee.component"
   styleUrls: ['./homepage.component.css']
 })
 export default class HomepageComponent {
+  images = [
+    '../../../assets/homepage/fraser.PNG',
+    '../../../assets/homepage/fraser2.jpg',
+    '../../../assets/homepage/fraser3.jpg',
+    '../../../assets/homepage/fraser4.jpg',
+    '../../../assets/homepage/fraser5.jpg',
+  ];
+  randomImage: string = '../../../assets/homepage/fraser.PNG';
+
   constructor(private router: Router) {}
 
   handleClick(url: string) {
     this.router.navigate([url]);
+  }
+
+  ngOnInit(): void {
+    this.pickRandomImage();
+  }
+
+  pickRandomImage(): void {
+    const randomIndex = Math.floor(Math.random() * this.images.length);
+    this.randomImage = this.images[randomIndex];
   }
 }
