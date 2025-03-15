f5463b17999e00467b8720198c35cd949bb2e2ba
frazzle <fraserbowen82@hotmail.co.uk>
2025-03-15 11:09:27 +0100

    another change to config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'FrasersWebsite',
-      script: $(which http-server),
+      script: '/run/user/1000/fnm_multishells/3531_1742024969347/bin/http-server',
       args: ['-p', '8080', '-d', 'false', './dist/frasers-website/'],
-      user: 'your_username',
+      user: 'fraserbowen',
     },
   ],
 };
