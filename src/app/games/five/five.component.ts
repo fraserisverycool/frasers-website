a878d29f65049d2822192e40ad3299dbfcbb8420
frazzle <fraserbowen82@hotmail.co.uk>
2025-03-23 00:39:46 +0100

    started refactoring the games

--- src/app/games/five/five.component.ts
+++ src/app/games/five/five.component.ts
@@ -1,6 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import GamesComponent from "../games.component";
+import GamesComponent from "../games.component"
+import { HttpClient } from "@angular/common/http";;
+
+interface Game {
+  name: string;
+  vibes: string;
+  gameplay: string;
+  review: string[];
+  image: string;
+  platform: string;
+  yearPlayed: string;
+}
 
 @Component({
   selector: 'app-five',
@@ -9,6 +20,23 @@ import GamesComponent from "../games.component";
   templateUrl: './five.component.html',
   styleUrls: ['./five.component.css']
 })
-export default class FiveComponent {
+export default class FiveComponent implements OnInit {
+  games: Game[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadGames();
+  }
+
+  loadGames(): void {
+    this.http.get<{ games: Game[] }>('assets/games/2025.json').subscribe({
+      next: (data) => {
+        this.games = data.games;
+      },
+      error: (err) => {
+        console.error('Failed to load games:', err);
+      },
+    });
+  }
 }
--- src/app/games/four/four.component.ts
+++ src/app/games/four/four.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import GamesComponent from "../games.component";
+import GamesComponent from "../games.component"
+import { HttpClient } from "@angular/common/http";;
+
+interface Game {
+  name: string;
+  vibes: string;
+  gameplay: string;
+  review: string[];
+  image: string;
+  platform: string;
+  yearPlayed: string;
+}
 
 @Component({
   selector: 'app-four',
   standalone: true,
-    imports: [CommonModule, GamesComponent],
+  imports: [CommonModule, GamesComponent],
   templateUrl: './four.component.html',
   styleUrls: ['./four.component.css']
 })
-export default class FourComponent {
+export default class FourComponent implements OnInit {
+  games: Game[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadGames();
+  }
+
+  loadGames(): void {
+    this.http.get<{ games: Game[] }>('assets/games/2024.json').subscribe({
+      next: (data) => {
+        this.games = data.games;
+      },
+      error: (err) => {
+        console.error('Failed to load games:', err);
+      },
+    });
+  }
 }
