60e6cbc27f6a6b4a20cf47568580f8c45c0017b4
frazzle <fraserbowen82@hotmail.co.uk>
2025-03-23 12:14:23 +0100

    refactored more games

--- src/app/games/zero/zero.component.ts
+++ src/app/games/zero/zero.component.ts
@@ -1,13 +1,42 @@
-import { Component, inject, signal } from '@angular/core';
-import { NgFor } from '@angular/common';
-import GamesComponent from "../games.component";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import GamesComponent from "../games.component"
+import { HttpClient } from "@angular/common/http";;
+
+interface Game {
+  name: string;
+  vibes: string;
+  gameplay: string;
+  review: string[];
+  image: string;
+  platform: string;
+  yearPlayed: string;
+}
 
 @Component({
-  selector: 'app-oranges',
+  selector: 'app-zero',
   standalone: true,
-    imports: [NgFor, GamesComponent],
+  imports: [CommonModule, GamesComponent],
   templateUrl: './zero.component.html',
   styleUrls: ['./zero.component.css']
 })
-export default class ZeroComponent {
+export default class ZeroComponent implements OnInit {
+  games: Game[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadGames();
+  }
+
+  loadGames(): void {
+    this.http.get<{ games: Game[] }>('assets/games/2020.json').subscribe({
+      next: (data) => {
+        this.games = data.games;
+      },
+      error: (err) => {
+        console.error('Failed to load games:', err);
+      },
+    });
+  }
 }
