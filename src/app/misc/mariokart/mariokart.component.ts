4e393a41ad49da14b4d835c4048cb0b17b8ac991
frazzle <fraserbowen82@hotmail.co.uk>
2025-05-05 00:33:12 +0200

    added three new mario kart games

--- src/app/misc/mariokart/mariokart.component.ts
+++ src/app/misc/mariokart/mariokart.component.ts
@@ -40,12 +40,12 @@ export default class MariokartComponent implements OnInit{
 
   gameDescription = {
     'snes': "The original and OG game! I never played this one as a child, instead just coming back to it as an adult. It's really hard but you can get the knack of it if you play for a bit. This is early days so they didn't have unique tracks, instead having Mario Circuit 1, 2 etc. In fact there are four Mario Circuits which is excessive. For this ranking I put all the themed circuits together even if there is some variation between the tracks because I wanted at least some level of consistency for this game that I have played the least out of all of the Mario Karts.",
-    'n64': 'Mario Kart 64',
+    'n64': "The Nintendo 64 Mario Kart was so fucking iconic. It created the Mario Kart vibe as we know it today. It is so nostalgic and just being from this game makes most tracks instant classics. Seeing these tracks remade so many times is a total pleasure because they are so special.",
     'gba': "Super Circuit is lowkey a super fun game. It's hard to get used to when going on 150cc yes but it's still a lot of fun! They take the Super Mario Kart formula and modernise it, also creating some really inspired themed circuits. The latter half of the tracks tend to have a lot of difficult 90 degree turns and other bullshit, which makes it less fun. For some reason, there are four Bowser's Castles. I ended up ranking them together because their vibes are so similar but that fourth one is just ridiculous.",
     'gcn': 'Mario Kart: Double Dash!!',
     'ds': 'Mario Kart DS',
     'wii': 'Mario Kart Wii',
-    '3ds': 'Mario Kart 7',
+    '3ds': "This is in my opinion the weakest Mario Kart. It doesn't bring much to the table in terms of gameplay, and definitely not when it comes to fun and unique racetracks. Music-wise we're working with an ugly, tinny-sounding set of instruments. I will admit that its selection of retro tracks is excellent though (as is reflected by this ranking). Full disclosure, I haven't played this one as much as other ones, in fact I never even owned it, but rather borrowed it from my friend Andrew for a few weeks.",
     'mk8': 'Mario Kart 8 Deluxe',
   };
 
