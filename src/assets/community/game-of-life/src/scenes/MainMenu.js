903d727d227f59e4af7be37b6daa4bec5f3e0b53
Fraser Bowen <fraser.bowen@kv.digital>
2025-05-07 18:08:45 +0200

    general content update

--- src/assets/community/game-of-life/src/scenes/MainMenu.js
+++ src/assets/community/game-of-life/src/scenes/MainMenu.js
@@ -16,7 +16,7 @@ export class MainMenu extends Scene
         this.tile.scaleY = this.scaleTileY;
         this.tile.setOrigin(0, 0);
 
-        this.add.text(512, 300, 'Super Shooter', {
+        this.add.text(512, 300, 'Game of Life', {
             fontFamily: 'Arial Black', fontSize: 70, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
