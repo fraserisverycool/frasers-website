aec6961c6b52bc604e68f639f6357710b9fb7835
Fraser Bowen <fraser.bowen@kv.digital>
2025-10-22 18:17:39 +0200

    fixed bug and added main and header tags

--- src/app/music/daily/daily.component.ts
+++ src/app/music/daily/daily.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {HttpClient} from "@angular/common/http";
 import {RatingService} from "../../utils/rating-bar/service/rating.service";
 import {DailySoundtrack} from "./daily-soundtrack.interface";
-import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
+import {DomSanitizer, SafeResourceUrl, SafeUrl} from "@angular/platform-browser";
 import {RatingBarComponent} from "../../utils/rating-bar/rating-bar.component";
 import {RouterLink} from "@angular/router";
 
@@ -29,14 +29,15 @@ export class DailyComponent implements OnInit {
   loadDailySoundtracks(): void {
     this.http.get<{ dailysoundtracks: DailySoundtrack[] }>('assets/music/daily/daily-soundtracks.json').subscribe({
       next: (data) => {
-        this.dailySoundtracks = data.dailysoundtracks.map(dailysoundtrack => ({
-          ...dailysoundtrack,
-          embed: this.sanitizer.bypassSecurityTrustResourceUrl(this.getEmbedUrl(dailysoundtrack.link))
-        }));
+        this.dailySoundtracks = data.dailysoundtracks;
         this.getRatings();
         this.today = this.dailySoundtracks.find(dailySoundtrack => dailySoundtrack.day === this.todaysDate()) || null;
         if (this.today) {
-          this.safeLink = this.today.embed;
+          if (typeof(this.today.link) === 'string') {
+            this.safeLink = this.getSafeEmbedUrl(this.today.link);
+          } else {
+            this.safeLink = this.getUnsafeEmbedUrl(this.today.link);
+          }
         }
       },
       error: (err) => {
@@ -70,7 +71,24 @@ export class DailyComponent implements OnInit {
   }
 
   private getEmbedUrl(link: string): string {
-    const videoId = link.split('v=')[1];
-    return `https://www.youtube.com/embed/${videoId}`;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const match = link.match(regExp);
+
+    if (match && match[2].length == 11) {
+      return `https://www.youtube.com/embed/${match[2]}`;
+    } else {
+      console.error(`Could not extract video ID for url: ${link}`);
+      return '';
+    }
+  }
+
+  private getSafeEmbedUrl(link: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.getEmbedUrl(link))
+  }
+
+  private getUnsafeEmbedUrl(link: SafeResourceUrl) {
+    let realUrl = (link as any).changingThisBreaksApplicationSecurity as string;
+    let embed = this.getEmbedUrl(realUrl);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(embed);
   }
 }
