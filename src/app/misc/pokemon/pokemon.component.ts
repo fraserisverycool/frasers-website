b027529ad0c85ac0fcf7e02fe5d91fe00dfa4daa
Fraser Bowen <fraser.bowen@kv.digital>
2025-11-18 01:20:53 +0100

    finishing touches

--- src/app/misc/pokemon/pokemon.component.ts
+++ src/app/misc/pokemon/pokemon.component.ts
@@ -1,9 +1,8 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {HttpClient} from "@angular/common/http";
 import {Pokemon, TierData} from "./pokemon.interface";
 import {CloseButtonComponent} from "../../utils/close-button/close-button.component";
-import { environment } from '../../../environments/environment';
 import {ImageService} from "../../utils/services/image.service";
 
 @Component({
@@ -16,6 +15,7 @@ import {ImageService} from "../../utils/services/image.service";
 export default class PokemonComponent  implements OnInit {
   @Output() close: EventEmitter<void> = new EventEmitter<void>();
 
+  originalTiers: TierData = {};
   tiers: TierData = {};
   tierHeadings: any = {
     SS: "Best of the best",
@@ -28,7 +28,7 @@ export default class PokemonComponent  implements OnInit {
     F: "Hate it, ugly"
   };
   orderedTiers: string[] = ["SS", "S", "A", "B", "C", "D", "E", "F"];
-
+  currentFilter: number = 0;
   collapsed: { [tier: string]: boolean } = {};
   selectedPokemon: Pokemon | null = null;
 
@@ -37,7 +37,8 @@ export default class PokemonComponent  implements OnInit {
   ngOnInit(): void {
     this.http.get<TierData>('assets/data/pokemon.json')
       .subscribe(data => {
-        this.tiers = data;
+        this.originalTiers = data;
+        this.tiers = this.originalTiers;
 
         for (let t of Object.keys(data)) {
           this.collapsed[t] = true;
@@ -55,5 +56,47 @@ export default class PokemonComponent  implements OnInit {
 
   closeModal() {
     this.selectedPokemon = null;
+  }
+
+  private generationRange(gen: number): { min: number; max: number } {
+    const ranges: Record<number, { min: number; max: number }> = {
+      1: { min: 1, max: 151 },
+      2: { min: 152, max: 251 },
+      3: { min: 252, max: 386 },
+      4: { min: 387, max: 493 },
+      5: { min: 494, max: 649 },
+      6: { min: 650, max: 721 },
+      7: { min: 722, max: 809 },
+      8: { min: 810, max: 905 },
+      9: { min: 906, max: 1025 }
+    };
+
+    return ranges[gen] ?? { min: 1, max: Infinity };
+  }
+
+  getFilteredTiers(): TierData {
+    if (!this.currentFilter) {
+      return this.tiers;
+    }
+
+    const { min, max } = this.generationRange(this.currentFilter);
+    const filtered: TierData = {};
+
+    for (const tier of Object.keys(this.tiers)) {
+      filtered[tier] = this.tiers[tier].filter(pokemon => {
+        const num = Number(pokemon.number);
+        return num >= min && num <= max;
+      });
+    }
+
+    return filtered;
+  }
+
+  filterPokemon(criteria: number): void {
+    this.currentFilter = criteria;
+    this.tiers = this.originalTiers;
+    if (criteria != 0) {
+      this.tiers = this.getFilteredTiers();
+    }
   }
 }
