515e9f0cc6356a12b02b20dca17548d5b40db6ff
frazzle <fraserbowen82@hotmail.co.uk>
2025-12-31 14:13:29 +0100

    Revert "upgrade to angular 21"
    This reverts commit 47e2086704da0bb63c65e1799f786d418c1edb47.

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,4 +1,3 @@
-import { provideZoneChangeDetection } from "@angular/core";
 /**
  * @license
  * Copyright Google LLC All Rights Reserved.
@@ -11,6 +10,6 @@ import { bootstrapApplication, BootstrapContext } from '@angular/platform-browse
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, {...config, providers: [provideZoneChangeDetection(), ...config.providers]}, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(AppComponent, config, context);
 
 export default bootstrap;
