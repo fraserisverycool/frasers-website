81ba0833512c94fad2d725f307152212da93a368
frazzle <fraserbowen82@hotmail.co.uk>
2026-01-30 13:59:47 +0100

    more work on back to brightwell

--- src/app/misc/brightwell/brightwell.component.ts
+++ src/app/misc/brightwell/brightwell.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { environment } from "../../../environments/environment";
+import {ImageService} from "../../utils/services/image.service";
 
 @Component({
   selector: 'app-brightwell',
@@ -8,11 +9,33 @@ import { environment } from "../../../environments/environment";
   templateUrl: './brightwell.component.html',
   styleUrl: './brightwell.component.css',
 })
-export default class BrightwellComponent {
+export default class BrightwellComponent implements OnInit, OnDestroy {
   path: string = `${environment.imageBaseUrl}/misc/brightwell/Brightwell.html`;
   safePath: SafeResourceUrl;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(private sanitizer: DomSanitizer, protected imageService: ImageService) {
     this.safePath = this.sanitizer.bypassSecurityTrustResourceUrl(this.path);
+  }
+
+  private messageHandler = (event: MessageEvent) => {
+    if (!event.data || event.data.type !== 'TWINE_EVENT') return;
+
+    switch (event.data.event) {
+      case 'ending1':
+        this.onEnding(event.data.data);
+        break;
+    }
+  };
+
+  ngOnInit() {
+    window.addEventListener('message', this.messageHandler);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('message', this.messageHandler);
+  }
+
+  onEnding(data: any) {
+    console.log('Test', data);
   }
 }
